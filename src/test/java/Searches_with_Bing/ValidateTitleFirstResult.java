cafca21ee8ae1d4baef9ad7e5cbea18a1993b04d
ivanvelchev <ivan.velchev.a61@learn.telerikacademy.com>
2024-09-25 14:38:10 +0300

    SeleniumWebDriverI

--- src/test/java/Searches_with_Bing/ValidateTitleFirstResult.java
+++ src/test/java/Searches_with_Bing/ValidateTitleFirstResult.java
@@ -5,43 +5,41 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.time.Duration;
 import java.util.List;
 
 public class ValidateTitleFirstResult {
 
     public static final String SEARCH_INPUT = "Telerik Academy Alpha";
+    public static final String EXPECTED_RESULT = "IT Career Start in 6 Months - Telerik Academy Alpha";
 
     @Test
 public void validateFirstResult(){
 
-    WebDriver driver = new FirefoxDriver();
-
+    WebDriver driver = new ChromeDriver();
     driver.get("https://www.bing.com");
-    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
 
+    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
     driver.manage().window().maximize();
 
 
     WebElement acceptButtonIsDisplayed = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("bnp_btn_accept")));
     WebElement acceptButton = driver.findElement(By.id("bnp_btn_accept"));
-
         acceptButton.click();
 
 
-        WebElement searchBox = driver.findElement(By.id("sb_form_q"));
-   // searchBox.click();
+    WebElement searchBox = driver.findElement(By.id("sb_form_q"));
         searchBox.sendKeys(SEARCH_INPUT + Keys.ENTER);
 
-     WebElement searchResultsBox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("b_tween_searchResults")));
+    WebElement searchResultsBox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("b_tween_searchResults")));
 
-        List<WebElement> resultLinks = driver.findElements(By.xpath("//h2/a"));
+    List<WebElement> resultLinks = driver.findElements(By.xpath("//h2/a"));
         var firstResult = resultLinks.get(0);
-        Assertions.assertEquals("IT Career Start in 6 Months - Telerik Academy Alpha", firstResult.getText());
+        
+    Assertions.assertEquals(EXPECTED_RESULT, firstResult.getText());
 
 
 driver.close();
