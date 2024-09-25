62a8faad86021e84dd1a1a7ac1ba76466e26281b
ivanvelchev <ivan.velchev.a61@learn.telerikacademy.com>
2024-09-25 14:38:10 +0300

    SeleniumWebDriverI

--- src/test/java/Searches_with_Google/ValidateTitleFirstResult.java
+++ src/test/java/Searches_with_Google/ValidateTitleFirstResult.java
@@ -1,15 +1,25 @@
 package Searches_with_Google;
-
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
 
 public class ValidateTitleFirstResult {
 
+
+
+    // Chrome Version 129.0.6668.71 (Official Build) (arm64).
+
     public static final String SEARCH_INPUT = "Telerik Academy Alpha";
+    public static final String EXPECTED_RESULT = "IT Career Start in 6 Months - Telerik Academy Alpha";
 
     @Test
     public void validateFirstTitle (){
@@ -17,25 +27,25 @@ public class ValidateTitleFirstResult {
     WebDriver driver = new ChromeDriver();
 
     driver.get("https://www.google.com");
-    driver.manage().window().fullscreen();
+    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+
+        driver.manage().window().maximize();
 
     WebElement cookieButton = driver.findElement(By.id("L2AGLb"));
     cookieButton.click();
 
     WebElement searchBox = driver.findElement(By.id("APjFqb"));
-    //searchBox.click();
     searchBox.sendKeys(SEARCH_INPUT + Keys.ENTER);
 
+    WebElement searchResultsBox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("search")));
+
+    List<WebElement> resultLinks = driver.findElements(By.xpath("//h3"));
+        var firstResult = resultLinks.get(0);
+
+    Assertions.assertEquals(EXPECTED_RESULT, firstResult.getText(), "This result discrepancy has been reported to Telerik Academy");
 
 
-
-
-
-
-driver.close();
-
-
-
+       driver.quit();
 
     }
 }
